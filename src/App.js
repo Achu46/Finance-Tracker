b4cec07e8b89ec182e252e68f121a4e63a62eada
Mohamed Asfaque <mohamedasfaque123@gmail.com>
2025-09-08 11:56:16 +0530

    Set up Firebase Firestore database and initial collections

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
-import  AppRoutes  from "../src/AppRoutes";
-import 'bootstrap/dist/css/bootstrap.min.css';
+import AppRoutes from "../src/AppRoutes";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { ToastContainer } from "react-toastify";
 
 function App() {
   return (
     <>
+      <ToastContainer />
       <AppRoutes />
     </>
   );
---
+++ src/Firebase.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDDfRJxwQ7nfXegTxUgR-pYpcq0fBVAfOw",
+  authDomain: "finance-tracker-achu.firebaseapp.com",
+  projectId: "finance-tracker-achu",
+  storageBucket: "finance-tracker-achu.appspot.com", // FIXED
+  messagingSenderId: "109107269974",
+  appId: "1:109107269974:web:8d4fae0d78b5dfbd3486f7",
+  measurementId: "G-GJF0HYJ8L1"
+};
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);

fa07747af84a3c3a85a2ea67e2d4c17ef959ea89
Mohamed Asfaque <mohamedasfaque123@gmail.com>
2025-09-08 11:56:16 +0530

    Set up Firebase Firestore database and initial collections

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -1,8 +1,77 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import { db } from "../Firebase"; // import firebase config
+import { collection, addDoc } from "firebase/firestore";
 import "../styles/signup.css";
+import { toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import bcrypt from "bcryptjs";
+import { query, where, getDocs } from "firebase/firestore";
 
 const Signup = () => {
+  const [formData, setFormData] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    mobile: "",
+    password: "",
+    confirmPassword: "",
+  });
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = async () => {
+    if (formData.password !== formData.confirmPassword) {
+      toast.error("⚠️ Password Mismatch");
+      return;
+    }
+
+    const hashedPassword=await bcrypt.hash(formData.password, 10);
+
+    try {
+      // 1. Email check
+      const emailQuery = query(
+        collection(db, "users"),
+        where("email", "==", formData.email)
+      );
+      const emailSnapshot = await getDocs(emailQuery);
+
+      if (!emailSnapshot.empty) {
+        toast.error("❌ Email already exists!");
+        return;
+      }
+
+      // 2. Mobile check
+      const mobileQuery = query(
+        collection(db, "users"),
+        where("mobile", "==", formData.mobile)
+      );
+      const mobileSnapshot = await getDocs(mobileQuery);
+
+      if (!mobileSnapshot.empty) {
+        toast.error("❌ Mobile number already exists!");
+        return;
+      }
+
+      // 3. If not exists → Insert
+      await addDoc(collection(db, "users"), {
+        firstName: formData.firstName,
+        lastName: formData.lastName,
+        email: formData.email,
+        mobile: formData.mobile,
+        password: hashedPassword, // NOTE: hash pannala! (production la hash pannanum)
+        createdAt: new Date(),
+      });
+
+      console.log("Data Inserted Successfully");
+      toast.success("🍾 Signup successful!");
+    } catch (error) {
+      console.error("Error: ", error);
+      toast.error("😭 Something went wrong");
+    }
+  };
   // Generate random stars
   const starCount = 150;
   const stars = Array.from({ length: starCount }, () => ({
@@ -41,7 +110,7 @@ const Signup = () => {
           maxWidth: "900px",
           width: "100%",
           background: "rgba(255,255,255,0.15)",
-        //   boxShadow: "0 8px 32px 0 rgba(31, 38, 135, 0.37)",
+          //   boxShadow: "0 8px 32px 0 rgba(31, 38, 135, 0.37)",
           backdropFilter: "blur(10px)",
           WebkitBackdropFilter: "blur(10px)",
           border: "1px solid rgba(255, 255, 255, 0.18)",
@@ -55,20 +124,29 @@ const Signup = () => {
           <div className="col-12 col-md-6">
             <input
               type="text"
+              name="firstName"
               className="form-control mb-3"
               placeholder="First Name"
+              value={formData.firstName}
+              onChange={handleChange}
               style={{ borderRadius: "10px", padding: "12px" }}
             />
             <input
               type="text"
+              name="lastName"
               className="form-control mb-3"
               placeholder="Last Name"
+              value={formData.lastName}
+              onChange={handleChange}
               style={{ borderRadius: "10px", padding: "12px" }}
             />
             <input
               type="email"
+              name="email"
               className="form-control"
               placeholder="Email"
+              value={formData.email}
+              onChange={handleChange}
               style={{ borderRadius: "10px", padding: "12px" }}
             />
           </div>
@@ -77,20 +155,29 @@ const Signup = () => {
           <div className="col-12 col-md-6">
             <input
               type="text"
+              name="mobile"
               className="form-control mb-3"
               placeholder="Mobile Number"
+              value={formData.mobile}
+              onChange={handleChange}
               style={{ borderRadius: "10px", padding: "12px" }}
             />
             <input
               type="password"
+              name="password"
               className="form-control mb-3"
               placeholder="Password"
+              value={formData.password}
+              onChange={handleChange}
               style={{ borderRadius: "10px", padding: "12px" }}
             />
             <input
               type="password"
+              name="confirmPassword"
               className="form-control"
               placeholder="Confirm Password"
+              value={formData.confirmPassword}
+              onChange={handleChange}
               style={{ borderRadius: "10px", padding: "12px" }}
             />
           </div>
@@ -107,7 +194,12 @@ const Signup = () => {
           <p className="mb-3 text-white">
             Already have an account? <Link to="#">Login</Link>
           </p>
-          <button className="btn btn-success btn-lg w-50">Sign up</button>
+          <button
+            onClick={handleSubmit}
+            className="btn btn-success btn-lg w-50"
+          >
+            Sign up
+          </button>
         </div>
       </div>
     </div>
